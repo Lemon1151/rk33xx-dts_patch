21e060711083fb44e26bf16fb50c55d6b45aed23
Lemon <149338935+Lemon1151@users.noreply.github.com>
2024-05-16 18:03:59 +0800

    Update customize-image.sh

--- userpatches/customize-image.sh
+++ userpatches/customize-image.sh
@@ -8,7 +8,7 @@ ARCH=$5
 
 set -e
 
-# Disable update
+# Disable update kernel
 mkdir -p /etc/apt/preferences.d
 DISABLE_UPDATE_CONF=/etc/apt/preferences.d/disable-update
 PKG_LIST=$(dpkg-query --show --showformat='${Package}\n')
@@ -20,6 +20,8 @@ function DISABLE_UPDATE() {
 cat /dev/null > ${DISABLE_UPDATE_CONF}
 DISABLE_UPDATE armbian-firmware
 DISABLE_UPDATE $(echo "${PKG_LIST}" | grep "armbian-bsp-cli-")
+DISABLE_UPDATE $(echo "${PKG_LIST}" | grep "^linux-image-")
+DISABLE_UPDATE $(echo "${PKG_LIST}" | grep "^linux-dtb-")
 DISABLE_UPDATE $(echo "${PKG_LIST}" | grep "^linux-u-boot")
 
 # Replace USTC mirror source
